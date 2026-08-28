61b51503c07ac0a287ad1034c0727e91218a3be2
KEVIN <farpistudent1@gmail.com>
2026-08-28 10:30:54 +0500

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 - Kirish
 app.use(express.static("public"));
@@ -14,11 +25,16 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 - bosqich Routing codelar
-app.get("/hello", function (req, res) {
-  res.end("<h1>Hello World by KEVIN</h1>");
+app.post("/create-item", (req, res) => {
+  // TODO: code with db here
 });
-app.get("/gift", function (req, res) {
-  res.end("<h1>Siz sovgalar sahifasidasiz</h1>");
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
