81628deabfe033db0323d891f01f042d4d363a88
KEVIN <farpistudent1@gmail.com>
2026-08-30 13:11:02 +0500

    fix: modify train.js with MIT task

--- train.js
+++ train.js
@@ -1,34 +1,56 @@
-/*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& 22. Asynchronous functionlarni qollash &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*/
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yaxshi talaba boling", //0-20
-  "togri boshliq tanlang va koproq xato qiling", // 20-30
-  "ozingizga ishlashni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq", // 60
-];
-//calback with interval
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
-  else {
-    setInterval(function () {
-      callback(null, list[5]);
-    }, 5000);
+/*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& MIT TASK A &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*/
+
+function countLetter(letter, word) {
+  let count = 0;
+  let i = 0;
+  while (i < word.length) {
+    if (word[i] === letter) {
+      /* string ichidagi har bir harfga index (raqam) beriladi.*/
+      count++;
+    }
+    i++;
   }
+  if (count === 0) {
+    return "Bunday harf bu so'zda qatnashmagan";
+  }
+  return count;
 }
 
-console.log("passed here 0");
-maslahatBering(65, (err, data) => {
-  if (err) console.log("ERROR", err);
-  console.log("javob:", data);
-});
-console.log("passed here 1");
+console.log(countLetter("b", "Uzbekistanbbbbb"));
+console.log(countLetter("A", "KevinAA"));
+console.log(countLetter("e", "engineer"));
+
+/*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& 22. Asynchronous functionlarni qollash &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*/
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba boling", //0-20
+//   "togri boshliq tanlang va koproq xato qiling", // 20-30
+//   "ozingizga ishlashni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq", // 60
+// ];
+// //calback with interval
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
+
+// console.log("passed here 0");
+// maslahatBering(65, (err, data) => {
+//   if (err) console.log("ERROR", err);
+//   console.log("javob:", data);
+// });
+// console.log("passed here 1");
 
 // Asynch function
 // async function maslahatBering(a, callback) {
