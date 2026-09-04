706d9f0b524db3d2214ce237586da102d7a5ec97
KEVIN <farpistudent1@gmail.com>
2026-09-04 13:29:39 +0900

    feat: mongoDB ulanish logiic

--- server.js
+++ server.js
@@ -1,46 +1,30 @@
-console.log("Web serverni boshlash");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
+const mongodb = require("mongodb");
 
-// 1 - Kirish
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+let db;
+const connectionSting =
+  "mongodb+srv://farpistudent1_db_user:XGVNYTiRsPR7kbZp@cluster0.y0cuoy5.mongodb.net/";
 
-// 2 - bosqich SESSION
-// 3 - bosqich Views
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 - bosqich Routing codelar
-app.post("/create-item", (req, res) => {
-  // TODO: code with db here
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running successfully on port : ${PORT}, http://localhost:${PORT}`,
-  );
-});
+mongodb.connect(
+  connectionSting,
+  {
+    usenewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection secceed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port : ${PORT}, http://localhost:${PORT}`,
+        );
+      });
+    }
+  },
+);
